ad68c4db41364c66deb31f1ad41e5f7ed94f6fc3
Ivan <devdnia@gmail.com>
2022-11-03 14:18:06 +0100

    Get implementado

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,17 +4,27 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
 
-    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+
+    // const usuarios = await Usuario.find(query)
+    //     .skip(Number( desde ))
+    //     .limit(Number( limite ));
+
+    // const total = await Usuario.countDocuments(query);   
+    
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number( desde ))
+            .limit(Number( limite ))
+    ])
 
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+        total,
+        usuarios
     });
 }
 
@@ -50,10 +60,7 @@ const usuariosPut = async(req, res) => {
     const usuario = await Usuario.findByIdAndUpdate( id, resto );
 
 
-    res.json({
-        msg: 'put API - controlador',
-        usuario
-    });
+    res.json( usuario );
 }
 const usuariosPatch = (req, res) => {
     res.json({
